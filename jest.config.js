13681f5f8571243879fd416ebf1a00f85d6c1e5c
Hector Coronado <hector.coronado@autodesk.com>
2020-09-13 16:52:36 -0700

    add identity-obj-proxy

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   // ascertain that jest is simulating a browser:
   testEnvironment: 'jest-environment-jsdom',
-  // import css modules!
   moduleNameMapper: {
+    // "mock" classNames that are set dynamically according
+    // to some CSS import:
+    '\\.module\\.css$': 'identity-obj-proxy',
+    // import css modules!
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
 }
