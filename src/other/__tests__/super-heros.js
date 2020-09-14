14cdcd8b9c82ba8ad56846f5009f94fb2b1f90f2
Hector Coronado <hector.coronado@autodesk.com>
2020-09-13 19:33:51 -0700

    run tests with snapshot

--- src/other/__tests__/super-heros.js
+++ src/other/__tests__/super-heros.js
@@ -1,14 +1,17 @@
 import {getFlyingSuperHeros} from '../super-heros'
 
 test('returns super heros that can fly', () => {
-  const flyingHerosArray = [
-    {name: 'Dynaguy', powers: ['disintegration ray', 'fly']},
-    {name: 'Apogee', powers: ['gravity control', 'fly']},
-    {name: 'Jack-Jack', powers: ['shape-shifting', 'fly']},
-  ]
+  // const flyingHerosArray = [
+  //   {name: 'Dynaguy', powers: ['disintegration ray', 'fly']},
+  //   {name: 'Apogee', powers: ['gravity control', 'fly']},
+  //   {name: 'Jack-Jack', powers: ['shape-shifting', 'fly']},
+  // ]
   // console.log(flyingHeros)
 
   const flyingHeros = getFlyingSuperHeros()
 
-  expect(flyingHeros).toEqual(flyingHerosArray)
+  // expect(flyingHeros).toEqual(flyingHerosArray)
+  // use snapshot to automate modification of
+  // our expected results:
+  expect(flyingHeros).toMatchSnapshot()
 })
