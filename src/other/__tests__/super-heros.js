767e56224399e44ed3d6987b6d610a47bd8eb709
Hector Coronado <hector.coronado@autodesk.com>
2020-09-14 08:53:24 -0700

    use toMatchInlineSnapshot instead of external snapshots

--- src/other/__tests__/super-heros.js
+++ src/other/__tests__/super-heros.js
@@ -13,5 +13,29 @@ test('returns super heros that can fly', () => {
   // expect(flyingHeros).toEqual(flyingHerosArray)
   // use snapshot to automate modification of
   // our expected results:
-  expect(flyingHeros).toMatchSnapshot()
+  expect(flyingHeros).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "name": "Dynaguy",
+        "powers": Array [
+          "disintegration ray",
+          "fly",
+        ],
+      },
+      Object {
+        "name": "Apogee",
+        "powers": Array [
+          "gravity control",
+          "fly",
+        ],
+      },
+      Object {
+        "name": "Jack-Jack",
+        "powers": Array [
+          "shape-shifting",
+          "fly",
+        ],
+      },
+    ]
+  `)
 })
